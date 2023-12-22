6386b2fa64d2c4aa6c36fc095dc4c7985fd55fb6
Ronin <154482296+roninmaster21@users.noreply.github.com>
2023-12-21 22:53:55 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='(package name)', # insert the name of your package here
-    version='(version)', # insert your version here using this format: 0.0.0. Packages will only be updated if this value is changed
+    name='example', # insert the name of your package here
+    version='0.0.1', # insert your version here using this format: 0.0.0. Packages will only be updated if this value is changed
     author='(name)', # insert your name here
     author_email='(email)', # insert your email here, this is so people can contact you
     description='(description)', # insert a description here
@@ -14,5 +14,5 @@ setuptools.setup(
     url='(link)', # insert the link to your GitHub repo here
     project_urls = {}, # insert necessary URLs here
     license='MIT', # license type
-    packages=['rfunctions'], # the directory to your package
+    packages=['src'], # the directory to your package
 )
