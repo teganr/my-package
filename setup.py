8411bd4e768818957647cbd8c9ff06e27dd77be9
teganr <154483170+teganr@users.noreply.github.com>
2023-12-21 23:15:21 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="github.com/teganr/my-package" # insert the link to your GitHub repo here
-    project_urls = {}, # insert necessary URLs here
+    #project_urls = {}, # insert necessary URLs here
     license='MIT', # license type
     packages=['teganmath'], # the directory to your package
 )
