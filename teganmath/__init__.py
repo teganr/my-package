b3e747523e7570a0dd9b630fcfb5516864293d97
teganr <154483170+teganr@users.noreply.github.com>
2023-12-21 23:17:03 -0600

    Update __init__.py

--- teganmath/__init__.py
+++ teganmath/__init__.py
@@ -1,1 +1,1 @@
-from .sum import * # repeat this line for each module and replace "example" with the module name. Do not leave out the dot at the beginning.
+from .addition import * # repeat this line for each module and replace "example" with the module name. Do not leave out the dot at the beginning.
